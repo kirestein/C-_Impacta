f854203d99f7144435daec758e1a89b0e88a147a
= <=>
2021-06-14 22:27:49 -0300

    Primeiros passos com a linguagem

--- Aula 1/Aula 1/Program.cs
+++ Aula 1/Aula 1/Program.cs
@@ -6,7 +6,14 @@ namespace Aula_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nome = "Erik";
+            var idade = 36;
+
+
+            if (idade >= 18)
+                Console.WriteLine("Olá " + nome + ", você é maior de idade, pode entrar!");
+            else
+                Console.WriteLine("Olá " + nome + ", você não é maior de idade, portanto não pode entrar!");
         }
     }
 }
